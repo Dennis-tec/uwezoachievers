16ce49384342be1307fdd27b11c3ec49efda345e
Dennis-tec <ykamakei6345@gmail.com>
2021-07-29 16:48:04 -0400

    add models to project folder

--- uwezoapp/admin.py
+++ uwezoapp/admin.py
@@ -1,3 +1,5 @@
+from uwezoachievers.uwezoapp.models import News
 from django.contrib import admin
 
 # Register your models here.
+admin.site.register(News)
--- uwezoapp/urls.py
+++ uwezoapp/urls.py
@@ -1,5 +1,10 @@
+from django.contrib.admin.decorators import register
+from django.conf.urls import url
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.index, name = 'index')
+    path('', views.index, name = 'index'),
+    path('login', views.login, name = 'login'),
+    path('register', views.register, name ='register'),
+    path('logout', views.logout, name = 'logout')
 ]
